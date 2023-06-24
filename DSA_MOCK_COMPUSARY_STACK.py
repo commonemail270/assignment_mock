ef456205df12b99ace3f02d665ed976edaed2a95
commonemail270 <121348616+commonemail270@users.noreply.github.com>
2023-06-25 00:23:18 +0530

    Update DSA_MOCK_COMPUSARY_STACK.py

--- DSA_MOCK_COMPUSARY_STACK.py
+++ DSA_MOCK_COMPUSARY_STACK.py
@@ -1,19 +1,26 @@
-stack = []
- 
+class Stack:
+    def __init__(self):
+        self.stack = []
 
-stack.append('a')
-stack.append('b')
-stack.append('c')
- 
-print('Initial stack')
-print(stack)
- 
+    def push(self, item):
+        self.stack.append(item)
 
-print('\nElements popped from stack:')
-print(stack.pop())
-print(stack.pop())
-print(stack.pop())
- 
-print('\nStack after elements are popped:')
-print(stack)
- 
+    def pop(self):
+        if not self.isEmpty():
+            return self.stack.pop()
+        else:
+            return None
+
+    def isEmpty(self):
+        return len(self.stack) == 0
+my_stack = Stack()
+
+my_stack.push(5)
+my_stack.push(10)
+my_stack.push(15)
+
+print(my_stack.pop())  # Output: 15
+print(my_stack.pop())  # Output: 10
+print(my_stack.isEmpty())  # Output: False
+print(my_stack.pop())  # Output: 5
+print(my_stack.isEmpty())  # Output: True
