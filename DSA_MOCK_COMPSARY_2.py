b7b9a0887f2acd7a1dea4954c599e77017d91c13
commonemail270 <121348616+commonemail270@users.noreply.github.com>
2023-06-25 00:22:34 +0530

    Update DSA_MOCK_COMPSARY_2.py

--- DSA_MOCK_COMPSARY_2.py
+++ DSA_MOCK_COMPSARY_2.py
@@ -1,18 +1,40 @@
-from collections import deque
- 
-stack = deque()
- 
-stack.append('a')
-stack.append('b')
-stack.append('c')
- 
-print('Initial stack:')
-print(stack)
+class Queue:
+    def __init__(self):
+        self.queue = []
 
-print('\nElements popped from stack:')
-print(stack.pop())
-print(stack.pop())
-print(stack.pop())
- 
-print('\nStack after elements are popped:')
-print(stack)
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+    def peek(self):
+        if not self.is_empty():
+            return self.queue[0]
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
+
+my_queue = Queue()
+
+my_queue.enqueue(5)
+my_queue.enqueue(10)
+my_queue.enqueue(15)
+
+print(my_queue.dequeue())  # Output: 5
+print(my_queue.dequeue())  # Output: 10
+print(my_queue.is_empty())  # Output: False
+print(my_queue.dequeue())  # Output: 15
+print(my_queue.is_empty())  # Output: True
+print(my_queue.peek())  # Output: None
+print(my_queue.size())  # Output: 0
